d5a4ffd7a84a64890a8740042174922230a5744e
Malushko Vadim <man.malushko@gmail.com>
2016-06-29 09:37:19 +0300

    aksjd

---
+++ WF2/Helper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WF2
+{
+    public static class Helper
+    {
+        /// <summary>
+        /// Parse string to int
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns>
+        /// If unable parse to int returns 0
+        /// </returns>
+        public static int GetInt(this string source)
+        {
+            int i;
+            if (int.TryParse(source, out i))
+            {
+                return i;
+            }
+            return -1;
+        }
+
+        public static BikeType GetSelectedBikeType(string source)
+        {
+            BikeType type = (BikeType)Enum.Parse(
+                typeof(BikeType),
+                source
+                );
+            return type;
+        }
+    }
+}
